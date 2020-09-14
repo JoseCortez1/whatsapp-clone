b1e9d84f702e00bbe8d0e82c5a14db858389b309
Jose Edardo C <Ctrlz-PC@localhost.localdomain>
2020-09-14 08:49:10 -0500

    view sidebar__chats

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,48 @@
 import React from 'react'
 import "./Sidebar.css"
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import {Avatar,  IconButton } from '@material-ui/core';
+
+
+//Components
+import SidebarChat from './SidebarChat'
 
 
 function Sidebar() {
     return (
         <div className="sidebar"> 
             
-            <div className="sidebar__herader">
+            <div className="sidebar__header">
+                <Avatar src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=976&q=80"/>
                 <div className="sidebar__headerRight">
-                    <DonutLargeIcon/>
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                    
                 </div>
+            </div>
+            <div className="sidebar__search"> 
+                <div className="sidebar__searchContainer">
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                    <input placeholder="Search or start new chat"  type="text"/>
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
---
+++ src/SidebarChat.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Avatar,  IconButton } from '@material-ui/core';
+import './SidebarChat.css'
+//https://youtu.be/gzdQDxzW2Tw?list=PLm0YJYRJ1JBKUQMjv1ESisikPLd2duU-y&t=4350
+function SidebarChat() {
+    return (
+        <div className="sidebarChat">
+            <Avatar src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=976&q=80"/>
+
+            <div className="sidebarChat__info">
+                <h2>
+                    room name
+                </h2>
+                <p>
+                    A message
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default SidebarChat
