76e82b025beca7d3be811e6084f8a3b106c6523b
Jose Eduardo Vazquez Cortez <50307754+JoseCortez1@users.noreply.github.com>
2021-01-02 23:53:58 -0600

    Update axios.js
    Update for Using online with backend mounted on heroku

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://localhost:9000/api/v1"
+    baseURL: "https://whatsapp-backend-jevc.herokuapp.com/"
 })
 
 export default instance
